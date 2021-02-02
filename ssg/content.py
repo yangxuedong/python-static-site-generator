e9f842deee7dd186d5e941f649686ef0e67c5088
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 11:54:30 -0600

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,35 @@
+import re
+from yaml import _win32sysloader as load
+from yaml import Fullloader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    def load(self, cls, string):
+        _, fm, content = split(cls.__regex, string, 2)
+        load(fm, Loader=Fullloader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data.append = {"content": content}
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        if type in self.data:
+            return self.data["type"]
+        else:
+            return None
+
+    def setter(self):
+        return self.type()
+
+    def __getitem__(self, item):
+        return self.data(item)
