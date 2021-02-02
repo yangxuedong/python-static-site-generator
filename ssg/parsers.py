62682d35cdd2b514b8ffb5bf501a941ac851460f
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 09:58:15 -0600

    Module 3

---
+++ ssg/parsers.py
@@ -0,0 +1,21 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser:
+    extensions = List[""]
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, 'r') as file:
+            return file.read()
+
+    def write(self, path, content, ext=".html"):
+        full_path = self.dest / path.with_suffix(ext).name
+        with open(full_path, 'w') as file:
+            file.write(content)
