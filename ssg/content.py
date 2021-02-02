4542dd0ee8392b9196ffbcac5430c91501d92e47
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 12:02:30 -0600

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -11,7 +11,7 @@ class Content(Mapping):
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        metadata = load(fm, Loader=Fullloader)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
